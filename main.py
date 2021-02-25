328e9cb30b1ade4d461690c3908dd4636b3e2c9e
Patryk Kotarski <73948640+Patrykkotarski812@users.noreply.github.com>
2021-02-25 20:28:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from math import *
+
+#Zad1:
+a = 2
+b = 3
+c = 3.5
+d = 2.2
+e = 'napis'
+f = 'tekst'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+#Zad2:
+a = 2
+b = 3
+
+dodawanie = a + b
+odejmowanie = a + b
+mnozenie = a * b
+dzielenie = a/b
+potegowanie = a ** b
+pierwiastkowanie = sqrt(a)
+
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
+print(potegowanie)
+print(pierwiastkowanie)
+
+#Zad3:
+
+#Zad4:
+
+
+#Zad5:
+imie = 'PATRYK'
+nazwisko = 'KOTARSKI'
+print (str.capitalize(imie) + " " + str.capitalize(nazwisko))
+
+#Zad6:
+cytat = 'I just can’t get you out of my head la la la la la la la '
+print(str.count(cytat, 'la'))
+
+
+#Zad7:
+zmienna = 'Grzegorz'
+print(zmienna[0])
+print(zmienna[7])
+
+#Zad8:
+print(str.split(cytat))
+
+
+#Zad9:
+pierwsza = 'slowo'
+druga = 2.5
+trzecia = 0x29
+print(type(pierwsza))
+print(type(druga))
+print(type(trzecia))
+print('pierwsza zmienna to %s, druga zmienna to %f, trzecia zmienna to %d' %(pierwsza, druga, trzecia))
+
+
+
